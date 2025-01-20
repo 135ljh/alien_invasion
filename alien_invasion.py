ab89e521e943eb46c02c12e374477baca52221e8
lijiahong <488241817@qq.com>
2025-01-20 13:21:07 +0800

    按Q键退出，无需鼠标关闭

--- alien_invasion.py
+++ alien_invasion.py
@@ -45,6 +45,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         """响应释放"""
@@ -52,6 +54,7 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+
 
 
     def _update_screen(self):
